3d67881b6fd51589ae07b00a93d8d819ae00a448
Taarak Rapolu <63356126+Taarak9@users.noreply.github.com>
2021-05-23 17:30:49 +0530

    Update loss_function.py

--- Activation_functions_and_Loss_functions/loss_function.py
+++ Activation_functions_and_Loss_functions/loss_function.py
@@ -3,16 +3,16 @@ import numpy as np
 def loss_function(name, y, y_hat, derivative=False):
   # y - target, y_hat - output
   if name == "mse":
-    if derivative:
-      return sum([ y_hat[i] - y[i] for i in range(len(y)) ])
-    else:
-      return sum([(y_hat[i] - y[i])**2 for i in range(len(y))])/ len(y)
+      if derivative:
+          return sum([ y_hat[i] - y[i] for i in range(len(y)) ])
+      else:
+          return sum([(y_hat[i] - y[i])**2 for i in range(len(y))])/ len(y)
 
   # y - target prob distro, y_hat - output prob distro
   # Cross Entropy
   if name == "ce":
-    if derivative:
-        # if activation fn is sigmoid/softmax
-        return sum([ y_hat[i] - y[i] for i in range(len(y)) ])      
-    else:
-        return -sum([y[i]*np.log(y_hat[i]) for i in range(len(y))])
+      if derivative:
+          # if activation fn is sigmoid/softmax
+          return sum([ y_hat[i] - y[i] for i in range(len(y)) ])      
+      else:
+          return -sum([y[i]*np.log(y_hat[i]) for i in range(len(y))])
