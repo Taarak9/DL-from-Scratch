5daf7037477cbf7210c9bafcc803273da75d0fbf
Taarak9 <taarak.rapolu@gmail.com>
2021-05-24 22:00:30 +0530

    update

--- Activation_functions_and_Loss_functions/activation_functions.py
+++ Activation_functions_and_Loss_functions/activation_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def activation_function(self, name, input, derivative=False):
+def activation_function(name, input, derivative=False):
   if name == "identity":
       if derivative:
           return np.ones_like(x)
@@ -8,20 +8,20 @@ def activation_function(self, name, input, derivative=False):
           return x
   elif name == "sigmoid":
       if derivative:
-          out = self.activation_function(name, input)
+          out = activation_function(name, input)
           return out * ( 1 - out )
       else:
           return 1 / (1 + np.exp(-input))
   elif name == "softmax":
       if derivative:
-          out = self.activation_function(name, input)
+          out = activation_function(name, input)
           return out * (1 - out)
       else:
           e_x = np.exp(input - np.max(input))
           return e_x / np.sum(e_x, axis=1, keepdims=True)
   elif name == "tanh":
       if derivative:
-          out = self.activation_function(name, input)
+          out = activation_function(name, input)
           return 1 - np.square(out)
       else:
           return 2 / (1 + np.exp(-2*input)) - 1
