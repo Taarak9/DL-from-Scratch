54eecfc0ba03cc7a909620a174f9812d3110f07a
Taarak9 <taarak.rapolu@gmail.com>
2021-05-24 11:23:52 +0530

    update activation and loss calls

--- Feedforward Neural Network/fnn.py
+++ Feedforward Neural Network/fnn.py
@@ -1,5 +1,7 @@
-import random
 import numpy as np
+import random
+from activation_functions import activation_function
+from loss_functions import loss_function
 
 # Feedforward Neural Network
 class FNN(object):
@@ -16,19 +18,14 @@ class FNN(object):
       a = sigmoid(np.dot(w, a) + b)
     return a
 
-  def cost_derivative(self, output_activations, y):
-    """Return the vector of partial derivatives \partial C_x /
-    \partial a for the output activations."""
-    return (output_activations-y)
-
   def evaluate(self, test_data):      
     test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
     return sum(int(x == y) for (x, y) in test_results)
 
   # Backpropagation
   def backprop(self, x, y):
-      gradient_b = [np.zeros(b.shape) for b in self.biases]
-      gradient_w = [np.zeros(w.shape) for w in self.weights]
+      nabla_b = [np.zeros(b.shape) for b in self.biases]
+      nabla_w = [np.zeros(w.shape) for w in self.weights]
 
       # feedforward
       activation = x
@@ -37,32 +34,32 @@ class FNN(object):
       for b, w in zip(self.biases, self.weights):
         z = np.dot(w, activation) + b
         zs.append(z)
-        activation = sigmoid(z)
+        activation = activation_function("sigmoid", z)
         activations.append(activation)
 
       # backward pass
-      delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
-      gradient_b[-1] = delta
-      gradient_w[-1] = np.dot(delta, activations[-2].transpose())
+      delta = loss_function("ce", y, activations[-1], True) * \
+            activation_function("sigmoid", zs[-1], True)
+      nabla_b[-1] = delta
+      nabla_w[-1] = np.dot(delta, activations[-2].transpose())
       for l in range(2, self.nlayers):
         z = zs[-l]
-        sp = sigmoid_prime(z)
+        sp = activation_function("sigmoid", z, True)
         delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
-        gradient_b[-l] = delta
-        gradient_w[-l] = np.dot(delta, activations[-l - 1].transpose())
-      return (gradient_b, gradient_w)
+        nabla_b[-l] = delta
+        nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+      return (nabla_b, nabla_w)
 
   def update_mini_batch(self, mini_batch, eta):
-      gradient_b = [np.zeros(b.shape) for b in self.biases]
-      gradient_w = [np.zeros(w.shape) for w in self.weights]
+      nabla_b = [np.zeros(b.shape) for b in self.biases]
+      nabla_w = [np.zeros(w.shape) for w in self.weights]
       for x, y in mini_batch:
-        delta_gradient_b, delta_gradient_w = self.backprop(x, y)
-        gradient_b = [nb + dnb for nb, dnb in zip(gradient_b, delta_gradient_b)]
-        gradient_w = [nw + dnw for nw, dnw in zip(gradient_w, delta_gradient_w)]
+        delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+        nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+        nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
 
-      self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, gradient_w)]
-      self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, gradient_b)]
+      self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+      self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
 
   # Stochastic Gradient decsent
   def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
@@ -77,10 +74,3 @@ class FNN(object):
         print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
       else:
         print("Epoch {0} complete".format(j))
-
-def sigmoid(z):
-    return 1.0/(1.0 + np.exp(-z))
-
-def sigmoid_prime(z):
-  """Derivative of the sigmoid function."""
-  return sigmoid(z) * (1 - sigmoid(z))
