37751367667ae6c7acf9ec36735bac7d1d9d5c2c
Taarak9 <taarak.rapolu@gmail.com>
2021-05-26 18:58:20 +0530

    list to array

--- Activation_functions_and_Loss_functions/loss_functions.py
+++ Activation_functions_and_Loss_functions/loss_functions.py
@@ -1,18 +1,19 @@
 import numpy as np
 
+# y and y_hat are numpy arrays
 def loss_function(name, y, y_hat, derivative=False):
   # y - target, y_hat - output
   # Mean Squared Error
   if name == "mse":
       if derivative:
-          return sum([ y_hat[i] - y[i] for i in range(len(y)) ])
+          return 
       else:
-          return sum([(y_hat[i] - y[i])**2 for i in range(len(y))])/ len(y)
+          return np.mean((y - y_hat)**2)
   # y - target prob distro, y_hat - output prob distro
   # Cross Entropy
   elif name == "ce":
       if derivative:
           # if activation fn is sigmoid/softmax
-          return sum([ y_hat[i] - y[i] for i in range(len(y)) ])      
+          return (y_hat - y)    
       else:
-          return -sum([y[i]*np.log(y_hat[i]) for i in range(len(y))])
+          return -np.sum(y * np.log(y_hat))
